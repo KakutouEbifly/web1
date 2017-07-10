a992837801dd06bc73d2d0c628539ecaef027300
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-07-10 17:47:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,29 @@
-//カウントダウンを表示するためのプログラム
+var time = 300;
+
+//毎秒処理の中身、カウントダウン
+var update = function(){
+  if(time === 0){
+    break;
+  } 
+  function downfall{
+    if(ret == true){
+      time -= 10;
+    }
+  }
+  time--;
+  refresh();
+}
+
+//update関数の処理時間設定
+var refresh = function(){
+  setTimeout(update, 1000);
+}
+update();
+
 
 //カウントダウンの時間を加速させるプログラム
+
+//カウントダウンを表示するためのプログラム
 
 //時計以外の装飾のためのプログラム
 
