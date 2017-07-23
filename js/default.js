43ab18301813f2ba770fe0b12d0f87374712e698
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-07-23 09:59:04 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -15,9 +15,9 @@ var Counter = function(){
   
   //分と秒を表示
   if(time < 60){
-    document.write("<p id=jikan>0:" + timeShow_S + "</p>");
+    document.write("<p id=jikan>" + "0:" + timeShow_S + "</p>");
   }else{
-    document.write("<p id=jikan_yaba>" + timeShow_M + ":" + timeShow_S + "</p>");
+    document.write("<p id=jikanYaba>" + timeShow_M + ":" + timeShow_S + "</p>");
   }
 }
 
