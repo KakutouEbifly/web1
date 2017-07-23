4082050d66a684d5001dc94c0e8039c106183082
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-07-23 18:58:35 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -20,7 +20,7 @@ var time = 300;
 //カウントダウン表示
 var Counter = function(){
   
-  //document.getElementById("jikan").style.display="block";
+  document.getElementById("jikan").style.display="block";
   
   var timeShow_M = 0;
   var timeShow_S;
@@ -48,7 +48,7 @@ var update = function(){
        break;
       } 
       time--;
-      Counter(time);
+      Counter();
       refresh();
   }while(true);
 }
